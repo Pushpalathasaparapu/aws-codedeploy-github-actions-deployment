28b0ded54cb2c9a58171c55ff626d94e2758b649
Pushpalathasaparapu <109743820+Pushpalathasaparapu@users.noreply.github.com>
2022-07-31 13:31:29 +0530

    Update after-install.sh

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -5,5 +5,3 @@ set -xe
 # Copy war file from S3 bucket to tomcat webapp folder
 cp /usr/share/spring-boot-hello-world-example/target/SpringBootHelloWorldExampleApplication.war /usr/share/tomcat8-codedeploy/webapps/SpringBootHelloWorldExampleApplication.war
 
-# Ensure the ownership permissions are correct.
-chown -R tomcat:tomcat /usr/share/tomcat8-codedeploy/webapps
