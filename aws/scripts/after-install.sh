f9985c48ce70ecc5673a57c2b8315e4c0c82be8b
Pushpalathasaparapu <109743820+Pushpalathasaparapu@users.noreply.github.com>
2022-07-31 10:14:34 +0530

    Update after-install.sh

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://aspirerepositorybucket/spring-boot-hello-world-example/target/SpringBootHelloWorldExampleApplication.war /usr/share/tomcat8-codedeploy/webapps/SpringBootHelloWorldExampleApplication.war
+aws s3 cp --s3-location bucket=aspirerepositorybucket,key=SpringBootHelloWorldExampleApplication.war,bundleType=zip /usr/share/tomcat8-codedeploy/webapps/SpringBootHelloWorldExampleApplication.war
 
 
 # Ensure the ownership permissions are correct.
